7c7a7f0e434e67b04e2cd5ace2efacf1ea3285a8
young-gratia <young.gratia@navercorp.com>
2019-03-15 15:47:09 +0900

    fix: challenge 1

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,25 @@
-//TODO: Implement hexToRgb
+const hexToRgb = (hex: string): { [key: string]: number } => {
+  if (hex.length !== 6 && hex.length !== 3) {
+    throw new Error('hex length must be 3 or 6!');
+  }
 
+  let result: { [key: string]: number } = { r: 0, g: 0, b: 0 };
+  const rgbValues: number[] = [0, 0, 0].map((value, i) => {
+    return hex.length === 6 ? parseInt(hex.slice(i * 2, (i + 1) * 2), 16) : parseInt(hex.slice(i, i + 1).repeat(2), 16);
+  });
 
-//TODO: Implement rgbToHex
+  Object.keys(result).map((color, index) => {
+    result[color] = rgbValues[index];
+  });
+
+  return result;
+};
+
+const rgbToHex = (r: number, g: number, b: number): string =>
+  [r, g, b].reduce(
+    (accumulator, currVal) =>
+      accumulator + (currVal >= 255 ? 'ff' : currVal <= 0 ? '00' : parseInt(currVal.toString(), 10).toString(16)),
+    ''
+  );
+
+export { hexToRgb, rgbToHex };
